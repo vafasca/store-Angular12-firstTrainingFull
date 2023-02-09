df068d6d5e53565475ff87134bd2e12f486714ab
Bruno Alejandro Ortiz Velasquez <counter_19_95@hotmail.com>
2023-02-09 15:58:07 -0400

    no content

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-@Component({//decorador component
+@Component({//decorador component, marca el comportamiento del componente
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
