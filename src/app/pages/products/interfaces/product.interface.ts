6fcef389a8d7bb8152bbb67d7eec5321509398d9
Bruno Alejandro Ortiz Velasquez <counter_19_95@hotmail.com>
2023-02-09 18:24:38 -0400

    adding c products and routing products, product interfaces

---
+++ src/app/pages/products/interfaces/product.interface.ts
@@ -0,0 +1,4 @@
+export interface Producto {
+    //campos del contrato segun el backend los datos que reciben
+
+}
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
